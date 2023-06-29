10375a96bd7a66e53f7ee84af8438d4955bb3bf6
Bryan Finster <bryan.finster@gmail.com>
2023-06-29 12:29:01 -0500

    style: More color updates

--- commitlint.config.js
+++ commitlint.config.js
@@ -4,7 +4,7 @@ module.exports = {
     'type-enum': [
       2,
       'always',
-      ['build', 'chore', 'ci', 'docs', 'feat', 'fix', 'perf', 'refactor', 'revert', 'style', 'test', 'button', 'feat!', 'fix!', 'refactor!'],
+      ['build', 'chore', 'ci', 'docs', 'feat', 'fix', 'perf', 'refactor', 'revert', 'style', 'test', 'feat!', 'fix!', 'refactor!'],
     ],
     'subject-case': [2, 'always', ['sentence-case', 'start-case', 'pascal-case', 'upper-case', 'lower-case']],
   },
