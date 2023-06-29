93ce0084f3d6f70508d469c0a325c8bb13861dca
Bryan Finster <bryan.finster@gmail.com>
2023-06-29 10:05:45 -0500

    fix: Enable major releases

--- commitlint.config.js
+++ commitlint.config.js
@@ -4,7 +4,7 @@ module.exports = {
     'type-enum': [
       2,
       'always',
-      ['build', 'chore', 'ci', 'docs', 'feat', 'fix', 'perf', 'refactor', 'revert', 'style', 'test', 'button'],
+      ['build', 'chore', 'ci', 'docs', 'feat', 'fix', 'perf', 'refactor', 'revert', 'style', 'test', 'button', 'feat!', 'fix!', 'refactor!'],
     ],
     'subject-case': [2, 'always', ['sentence-case', 'start-case', 'pascal-case', 'upper-case', 'lower-case']],
   },
