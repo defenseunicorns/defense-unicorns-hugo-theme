99931aab6607475811e036610f5407958ab8722b
Bryan Finster <bryan.finster@gmail.com>
2023-07-24 16:01:40 -0500

    fix: create typeahead search

--- assets/js/base.js
+++ assets/js/base.js
@@ -14,47 +14,47 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-;(function ($) {
-  'use strict'
+(function ($) {
+  "use strict";
 
   $(function () {
-    $('[data-bs-toggle="tooltip"]').tooltip()
-    $('[data-bs-toggle="popover"]').popover()
+    $('[data-bs-toggle="tooltip"]').tooltip();
+    $('[data-bs-toggle="popover"]').popover();
 
-    $('.popover-dismiss').popover({
-      trigger: 'focus',
-    })
-  })
+    $(".popover-dismiss").popover({
+      trigger: "focus",
+    });
+  });
 
   function bottomPos(element) {
-    return element.offset().top + element.outerHeight()
+    return element.offset().top + element.outerHeight();
   }
 
   // Bootstrap Fixed Header
   $(function () {
-    var promo = $('.js-td-cover')
+    var promo = $(".js-td-cover");
     if (!promo.length) {
-      return
+      return;
     }
 
-    var promoOffset = bottomPos(promo)
-    var navbarOffset = $('.js-navbar-scroll').offset().top
+    var promoOffset = bottomPos(promo);
+    var navbarOffset = $(".js-navbar-scroll").offset().top;
 
-    var threshold = Math.ceil($('.js-navbar-scroll').outerHeight())
+    var threshold = Math.ceil($(".js-navbar-scroll").outerHeight());
     if (promoOffset - navbarOffset < threshold) {
-      $('.js-navbar-scroll').addClass('navbar-bg-onscroll')
+      $(".js-navbar-scroll").addClass("navbar-bg-onscroll");
     }
 
-    $(window).on('scroll', function () {
-      var navtop = $('.js-navbar-scroll').offset().top - $(window).scrollTop()
-      var promoOffset = bottomPos($('.js-td-cover'))
-      var navbarOffset = $('.js-navbar-scroll').offset().top
+    $(window).on("scroll", function () {
+      var navtop = $(".js-navbar-scroll").offset().top - $(window).scrollTop();
+      var promoOffset = bottomPos($(".js-td-cover"));
+      var navbarOffset = $(".js-navbar-scroll").offset().top;
       if (promoOffset - navbarOffset < threshold) {
-        $('.js-navbar-scroll').addClass('navbar-bg-onscroll')
+        $(".js-navbar-scroll").addClass("navbar-bg-onscroll");
       } else {
-        $('.js-navbar-scroll').removeClass('navbar-bg-onscroll')
-        $('.js-navbar-scroll').addClass('navbar-bg-onscroll--fade')
+        $(".js-navbar-scroll").removeClass("navbar-bg-onscroll");
+        $(".js-navbar-scroll").addClass("navbar-bg-onscroll--fade");
       }
-    })
-  })
-})(jQuery)
+    });
+  });
+})(jQuery);
